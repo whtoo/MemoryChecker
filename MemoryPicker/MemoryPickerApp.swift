01013ac9bb9236b65545ec3502aee959586e5b46
whtoo <whtoo@qq.com>
2021-01-06 14:05:36 +0800

    Initial Commit

---
+++ MemoryPicker/MemoryPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryPickerApp.swift
+//  MemoryPicker
+//
+//  Created by blitz on 2021/1/6.
+//
+
+import SwiftUI
+
+@main
+struct MemoryPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
