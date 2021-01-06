829f968526fa04876640e8c75929a628859352b0
whtoo <whtoo@qq.com>
2021-01-06 20:06:23 +0800

    1. add geometry support

--- MemoryPicker/CardView.swift
+++ MemoryPicker/CardView.swift
@@ -9,22 +9,30 @@ import SwiftUI
 
 struct CardView: View {
     var card : MemoryGame<String>.Card
+    
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
-            
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
         }
     }
+    
+    func body(for size: CGSize) ->  some View {
+        
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: 10.0).fill()
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text(card.content)
+                } else {
+                    RoundedRectangle(cornerRadius: 10.0).fill()
+                }
+                
+            }.font(Font.system(size: min(size.width,size.height) * 0.75))
+       
+    }
 }
-
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView(card: MemoryGame<String>.Card(isFaceUp: false, isMatched: false, content: "👻",id: 0))
+        CardView(card: MemoryGame<String>.Card(isFaceUp: true, isMatched: false, content: "👻",id: 0)).foregroundColor(.blue)
     }
 }
